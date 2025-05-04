67e1be02879e08fdcd78c47f61b8f405c9c6c0e5
davisbasha <dbasha@my.ccsu.edu>
2025-05-04 13:28:47 -0400

    Add files via upload

---
+++ Number Generator.py
@@ -0,0 +1,19 @@
+# This program displays a random number
+
+# in the range of 1 through 100.
+
+import random
+
+def main():
+
+    # Get a random number.
+
+    number = random.randint(1, 10)
+
+     # Display the number.
+
+    print(f'The number is {number}.')
+
+ # Call the main function.
+
+main()
